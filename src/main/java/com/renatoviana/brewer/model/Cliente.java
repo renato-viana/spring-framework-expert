7310809fae5a4a4fafea819031ab09716cd7abd2
Renato Viana <renatoviana30@gmail.com>
2020-11-06 14:04:13 -0300

    controller/ClientesController e model/Cliente

---
+++ src/main/java/com/renatoviana/brewer/model/Cliente.java
@@ -0,0 +1,17 @@
+package com.renatoviana.brewer.model;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Cliente {
+	
+	@NotBlank
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
